846b0a1b0690008b0a50904ccbea635d195d84dc
Edwin Hurtado <edwin.hurtado@intercom.com.co>
2014-12-23 16:35:39 -0500

    Iconos

--- src/com/eventorio/app/MainActivity.java
+++ src/com/eventorio/app/MainActivity.java
@@ -1,20 +1,32 @@
 package com.eventorio.app;
 
 import java.util.ArrayList;
+
+import com.eventorio.app.utils.MyTextView;
+
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.TabHost.TabContentFactory;
+import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends ActionBarActivity {
 
 	
 	private ArrayList<String> specs;
+	private FragmentTabHost mTabHost;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		
 		initComponents();
@@ -29,34 +41,49 @@ public class MainActivity extends ActionBarActivity {
     		specs.add(aLabels[i]);
     	}
     	
-    	FragmentTabHost mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+    	mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
 		mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
-		mTabHost.newTabSpec(specs.get(0));
-		mTabHost.addTab(mTabHost.newTabSpec(specs.get(0)).setIndicator(specs.get(0)), HomeFragment.class, null);
 		
-		mTabHost.newTabSpec(specs.get(1));
-		mTabHost.addTab(mTabHost.newTabSpec(specs.get(1)).setIndicator(specs.get(1)), ProfileFragment.class, null);
 		
-		mTabHost.newTabSpec(specs.get(2));
-		mTabHost.addTab(mTabHost.newTabSpec(specs.get(2)).setIndicator(specs.get(2)), EventsFragment.class, null);
+		TabSpec tabSpec = getTabspec(mTabHost.getContext(), specs.get(0), R.layout.home_selector);
+		mTabHost.addTab(tabSpec,  HomeFragment.class, null);
 		
-		mTabHost.newTabSpec(specs.get(3));
-		mTabHost.addTab(mTabHost.newTabSpec(specs.get(3)).setIndicator(specs.get(3)), CalendarFragment.class, null);
-        
+		tabSpec = getTabspec(mTabHost.getContext(), specs.get(1), R.layout.profile_selector);
+		mTabHost.addTab(tabSpec,  ProfileFragment.class, null);
+		
+		tabSpec = getTabspec(mTabHost.getContext(), specs.get(2), R.layout.events_selector);
+		mTabHost.addTab(tabSpec, EventsFragment.class, null);
+		
+		tabSpec = getTabspec(mTabHost.getContext(), specs.get(3), R.layout.calendar_selector);
+		mTabHost.addTab(tabSpec, CalendarFragment.class, null);
+	}
+
+	private TabSpec getTabspec(Context context, String string, int homeSelector) {
+		View tabview = createTabView(context, string, homeSelector);
+		
+		TabSpec setContent = mTabHost.newTabSpec(string).setIndicator(tabview).setContent(new TabContentFactory() {
+			public View createTabContent(String tag) {
+				MyTextView tv = new MyTextView(MainActivity.this);
+				return tv;}
+		});
+		return setContent;
+	}
+
+	private static View createTabView(final Context context, final String text, int layout) {
+		View view = LayoutInflater.from(context).inflate(layout, null);
+		TextView tv = (TextView) view.findViewById(R.id.tabsText);
+		tv.setText(text);
+		return view;
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
